cf092ea062ad313b78db460400b2a3c201f65cd6
CodeRain49 <coderain49@gmail.com>
2025-09-24 15:02:49 +0700

    Update_Adder
    Isnan help me 99 percent

--- cal.py
+++ cal.py
@@ -1,6 +1,22 @@
 """calculate"""
-txt = input()
-a = int(input())
-b = int(input())
-if txt == "+":
-    print(a + b)
+
+def adder_ab(a, b):
+    '''a + b'''
+    return print(a + b)
+
+def main():
+    '''
+    txt -> operator
+    a, b -> operand
+    '''
+    txt = input()
+    a = int(input())
+    b = int(input())
+    result = "Sorry, I can't do this anymore...T_T"
+
+    if txt == "+":
+        result = adder_ab(a, b)
+
+    print(result)
+
+main()
